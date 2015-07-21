40e41b2478f53ad7a6e6e9b252d25d9f0f3164d5
Joshua Hansel <joshua.hansel89@gmail.com>
2015-07-21 16:19:16 -0500

    Beginning my new application

---
+++ include/base/TurkeyApp.h
@@ -0,0 +1,22 @@
+#ifndef TURKEYAPP_H
+#define TURKEYAPP_H
+
+#include "MooseApp.h"
+
+class TurkeyApp;
+
+template<>
+InputParameters validParams<TurkeyApp>();
+
+class TurkeyApp : public MooseApp
+{
+public:
+  TurkeyApp(const std::string & name, InputParameters parameters);
+  virtual ~TurkeyApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* TURKEYAPP_H */
---
+++ src/base/TurkeyApp.C
@@ -0,0 +1,54 @@
+#include "TurkeyApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+
+template<>
+InputParameters validParams<TurkeyApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+
+  params.set<bool>("use_legacy_uo_initialization") = false;
+  params.set<bool>("use_legacy_uo_aux_computation") = false;
+  return params;
+}
+
+TurkeyApp::TurkeyApp(const std::string & name, InputParameters parameters) :
+    MooseApp(name, parameters)
+{
+  srand(processor_id());
+
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  TurkeyApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+  TurkeyApp::associateSyntax(_syntax, _action_factory);
+}
+
+TurkeyApp::~TurkeyApp()
+{
+}
+
+// External entry point for dynamic application loading
+extern "C" void TurkeyApp__registerApps() { TurkeyApp::registerApps(); }
+void
+TurkeyApp::registerApps()
+{
+  registerApp(TurkeyApp);
+}
+
+// External entry point for dynamic object registration
+extern "C" void TurkeyApp__registerObjects(Factory & factory) { TurkeyApp::registerObjects(factory); }
+void
+TurkeyApp::registerObjects(Factory & factory)
+{
+}
+
+// External entry point for dynamic syntax association
+extern "C" void TurkeyApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { TurkeyApp::associateSyntax(syntax, action_factory); }
+void
+TurkeyApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+}
